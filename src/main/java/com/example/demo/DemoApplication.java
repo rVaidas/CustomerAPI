67590c1a8ebccb936842cdbca4e83d8ed1ac6222
vaidas.ronkaitis <vaidas.ronkaitis@gmail.com>
2022-05-14 17:45:17 +0300

    Added Customer class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Customer> hello(){
+		return List.of(new Customer(1, "John", "Bonham", LocalDate.of(1948, Month.AUGUST, 31), "911", "bambam@zeppelin.com"));
 	}
 
 }
---
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -0,0 +1,91 @@
+package com.example.demo.customer;
+
+import java.time.LocalDate;
+
+public class Customer {
+     private Integer id;
+     private String name;
+     private String surname;
+     private LocalDate birthDate;
+     private String telNumber;
+     private String email;
+
+     public Customer(){};
+
+     public Customer(int id, String name, String surname, LocalDate birthDate, String telNumber, String email) {
+          this.id = id;
+          this.name = name;
+          this.surname = surname;
+          this.birthDate = birthDate;
+          this.telNumber = telNumber;
+          this.email = email;
+     }
+
+     public Customer(String name, String surname, LocalDate birthDate, String telNumber, String email) {
+          this.name = name;
+          this.surname = surname;
+          this.birthDate = birthDate;
+          this.telNumber = telNumber;
+          this.email = email;
+     }
+
+     public int getId() {
+          return id;
+     }
+
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public String getSurname() {
+          return surname;
+     }
+
+     public void setSurname(String surname) {
+          this.surname = surname;
+     }
+
+     public LocalDate getBirthDate() {
+          return birthDate;
+     }
+
+     public void setBirthDate(LocalDate birthDate) {
+          this.birthDate = birthDate;
+     }
+
+     public String getTelNumber() {
+          return telNumber;
+     }
+
+     public void setTelNumber(String telNumber) {
+          this.telNumber = telNumber;
+     }
+
+     public String getEmail() {
+          return email;
+     }
+
+     public void setEmail(String email) {
+          this.email = email;
+     }
+
+     @Override
+     public String toString() {
+          return "Customer{" +
+                  "id=" + id +
+                  ", name='" + name + '\'' +
+                  ", surname='" + surname + '\'' +
+                  ", birthDate='" + birthDate + '\'' +
+                  ", telNumber='" + telNumber + '\'' +
+                  ", email='" + email + '\'' +
+                  '}';
+     }
+}
